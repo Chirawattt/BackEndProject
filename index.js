060097e1727b43cd4aa7cb493d2e1304f5e90b1b
Chirawattt <chirawat.yana@hotmail.com>
2024-02-22 13:10:32 +0700

    update cart add and delete

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json()); // format to transfer data in json
 app.use(session({
     secret:"secret",
     resave:false,
-    saveUninitialized:false
+    saveUninitialized:true
 }));
 
 // create a connection to the database
@@ -111,10 +111,6 @@ const Cart = sequelize.define('cart', {
         type: Sequelize.STRING,
         allowNull: false
     },
-    status: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
 });
 
 // Define the CartDetail model
@@ -125,11 +121,11 @@ const CartDetail = sequelize.define('cartDetail', {
         primaryKey: true
     },
     cart_id: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         allowNull: false
     },
     bread_id: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         allowNull: false
     },
     quantity: {
@@ -202,41 +198,87 @@ async function authenUser(username, password){
         })
         // If a user is found 
         if (user) {
-            // Check if the provided password matches the stored password
-            if (user.password === password) {
-                // Password is correct, return the user
-                return user;
+            if (user.password === password) { // Check if the provided password matches the stored password
+                return user; // Password is correct, return the user
             }else {
-                // Password is incorrect, return a message
-                return "!password";
+                return "!password"; // Password is incorrect, return a message
             }
         } else {
-            // User not found, return a message
-            return "!username&!password";
+            return "!username&!password"; // User not found, return a message
         }
     } catch(error) {
         console.log('Error querying database:', error);
         throw error;
     }
-
 }
 
-// route to create an account
-app.post('/user/register', (req, res) => {
-    User.findOne({
-        where: {username: req.body.username}
-    }).then(data => {
-        if (data) res.send('Your username is already used!\nPlease try another username.')
-        else {
-            User.create(req.body).then(user => {
-                res.json(user);
-            }).catch(err => {
-                res.status(500).send(err);
-            });
-        }
+// route to get all user
+app.get('/user/getUser', (req, res) => {
+    User.findAll().then(data => {
+        if (data) res.json(data);
+        else res.status(404).send("Can't find all user");
     }).catch(err => {
         res.status(500).send(err);
     });
+});
+
+// route to get user information by id
+app.get('/user/getUser/:id', (req,res) => {
+    User.findByPk(req.params.id).then(data => {
+        if (data) res.json(data);
+        else res.status(404).send("Doesn't have data of this user");
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+// route to create an account
+app.post('/user/register', (req, res) => {
+    try {
+        User.findAll().then(data => {
+            let Alldata = [];
+            let registerValid = true;
+            let strError = "";
+            if (data) {
+                data.forEach(user => {
+                    Alldata.push(user.dataValues.username);
+                    Alldata.push(user.dataValues.email);
+                    Alldata.push(user.dataValues.phone);
+                });
+
+                if (Alldata.includes(req.body.username)) {
+                   strError = `\nUsername: "${req.body.username}" is already use`;
+                   registerValid = false;
+                }if (Alldata.includes(req.body.email)) {
+                    strError += `\nemail: "${req.body.email}" is already use`;
+                    registerValid = false;
+                }if (Alldata.includes(req.body.phone)) {
+                    strError += `\nPhone number: "${req.body.phone}" is already use`;
+                    registerValid = false;
+                }
+                
+                if (registerValid) {
+                    User.create(req.body).then(data => {
+                        Cart.create({ // Generate Cart for new user
+                            user_id: data.dataValues.userId
+                        }).then(() => {
+                            res.send(`Already generated cart for User: '${data.dataValues.username}'\nRegister Successfully!`);
+                        }).catch(err => {
+                            res.status(500).send(err);
+                        });
+                    }).catch(err => {
+                        res.status(500).send(err);
+                    });
+                }else {
+                    res.send('Register Unsuccessfully!' + strError + '\nPlease try again!');
+                }
+            } else res.send('Could not find except User');
+        }).catch(err => {
+            res.status(500).send(err);
+        })
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 // route to login an account by using post
@@ -254,24 +296,85 @@ app.post('/user/login', async (req, res) => {
     }
 });
 
-// route to get user information
-app.get('/user/getData', (req,res) => {
-    User.findOne({
-        where: {userId: req.session.userId}
-    }).then(data => {
-        if(data) res.json(data);
-        else res.send("Doesn't have data of this user");
-    }).catch(err => {
-        res.status(500).send(err);
-    })
-});
-
 // route to update data of User
 app.post('/user/update/:id', (req, res) => {
-    // User.findByPk(req.params.id).then(data => {
-        
-    // })
-})
+    try {
+        User.findAll({
+            where: {userId: {[Sequelize.Op.ne]: req.params.id}}
+        }).then(data => {
+            let Alldata = [];
+            let updateValid = true;
+            let strError = "";
+            if (data) {
+                data.forEach(user => {
+                    Alldata.push(user.dataValues.username);
+                    Alldata.push(user.dataValues.email);
+                    Alldata.push(user.dataValues.phone);
+                });
+
+                if (Alldata.includes(req.body.username)) {
+                   strError = `\nUsername: "${req.body.username}" is already use`;
+                   updateValid = false;
+                }if (Alldata.includes(req.body.email)) {
+                    strError += `\nemail: "${req.body.email}" is already use`
+                    updateValid = false;
+                }if (Alldata.includes(req.body.phone)) {
+                    strError += `\nPhone number: "${req.body.phone}" is already use`
+                    updateValid = false;
+                }
+                
+                if (updateValid) {
+                    User.findByPk(req.params.id).then(user => {
+                        user.update(req.body);
+                        res.send('Update profile successfully.');
+                    }).catch(err => {
+                        res.status(500).send(err);
+                    });
+                }else {
+                    res.send('Update Profile invalid' + strError + '\nPlease try again!');
+                }
+            } else res.send('Could not find except User');
+        }).catch(err => {
+            res.status(500).send(err);
+        })
+    } catch (err) {
+        res.status(500).send('Error');
+    }
+});
+
+// route to delete user data
+app.delete('/user/delete/:id', (req, res) => {
+    User.findByPk(req.params.id).then(data => {
+        if (data) {
+            res.send(`Account: ${data.username} has been deleted`)
+            data.destroy().catch(err => {
+                res.status(500).send(err);
+            });
+        }else res.send(`Does't have these id in system.`);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+// route to get all bread
+app.get('/bread/all', (req, res) =>{
+    Bread.findAll().then(data => {
+        if(data) res.json(data);
+        else res.send("All bread not found");
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+// route to get some bread by id
+app.get('/bread/get/:id', (req, res) => {
+    Bread.findByPk(req.params.id).then(data => {
+        if (data) res.json(data);
+        else res.send('Not found this bread');
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
 
 // route to create data of Bread
 app.post('/bread/new', (req, res) => {
@@ -288,33 +391,100 @@ app.post('/bread/new', (req, res) => {
         }
     }).catch(err => {
         res.status(500).send(err);
-    })
+    });
 });
 
-// route to get all bread
-app.get('/bread/all', (req, res) =>{
-    Bread.findAll().then(data => {
-        if(data) res.json(data);
-        else res.send("All bread not found");
-    }).catch(err => {
-        res.status(500).send(err);
-    })
-});
+// route to update data of bread
+app.post('/bread/update/:id', (req, res) => {
+    try {
+        Bread.findAll({
+            where: {breadId: {[Sequelize.Op.ne]: req.params.id}}
+        }).then(data => {
+            let Alldata = [];
+            let updateValid = true;
+            let strError = "";
+            if (data) {
+                data.forEach(bread => {
+                    Alldata.push(bread.dataValues.name);
+                });
 
+                if (Alldata.includes(req.body.name)) {
+                   strError = `\nBread name: "${req.body.name}" is already use`;
+                   updateValid = false;
+                }
+                
+                if (updateValid) {
+                    Bread.findByPk(req.params.id).then(bread => {
+                        bread.update(req.body);
+                        res.send('Update Bread successfully.');
+                    }).catch(err => {
+                        res.status(500).send(err);
+                    });
+                }else {
+                    res.send('Update Bread invalid' + strError + '\nPlease try again!');
+                }
+            } else res.send('Could not find except Bread');
+        }).catch(err => {
+            res.status(500).send(err);
+        })
+    } catch (err) {
+        res.status(500).send('Error');
+    }
+});
 // route to delete a bread
 app.delete('/bread/delete/:id', (req, res) =>{
     Bread.findByPk(req.params.id).then(data => {
         if(data) {
-            data.destroy().then(() =>{
-                res.send(`${data.name} has been delete from Bread store`);
-            }).catch(err => {
+            res.send(`${data.name} has been delete from Bread store`);
+            data.destroy().catch(err => {
                 res.status(500).send(err);
-            })
+            });
         }else res.status(404).send("Not found this bread");
     }).catch(err => {
         res.status(500).send(err);
+    });
+});
+
+
+// route to add bread to cart
+app.post('/cart/add/:breadId', async (req, res) => { // req.body => cart_id, quantity
+    // Prase string breadId to Integer
+    breadId = await parseInt(req.params.breadId);
+    // Calculate subtotal of bread
+    subtotal = await Bread.findByPk(breadId).then(data => {
+        if (data) {
+            return data.price * req.body.quantity;
+        }else return 0;
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+
+    CartDetail.create({
+        cart_id: req.body.cart_id,
+        bread_id: breadId,
+        quantity: req.body.quantity,
+        subtotal: subtotal
+    }).then(data => {
+        if (data) res.json(data);
+        else res.send("Cannot add bread into cart!");
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+// Test some route
+app.delete('/cart/delete/:cartDetailId', (req, res) => {
+    CartDetail.findByPk(req.params.cartDetailId).then(data => {
+        if (data) {
+            Bread.findByPk(data.bread_id).then(breadData => {
+                res.send(`Deleted bread name: ${breadData.name}\nQuantity: ${data.quantity}\nOut of Cart!`);
+            }).catch(err => {res.status(500).send(err);})
+            data.destroy().catch(err => {res.status(500).send(err);})
+        }else res.send("Can not delete cart detail");
+
     })
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
